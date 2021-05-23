b84f0971634209f25a8176330a32e78bcc000a09
Frederick Johnson <fritzj96@gmail.com>
2021-05-23 11:21:59 -0500

    added: more routes & a 404 redirect

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,13 +1,23 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import { INDEX_ROUTE } from '.';
-import { BLOG_INDEX_ROUTE, BLOG_POST_ROUTE } from './routes';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import {
+  INDEX_ROUTE,
+  CONTACT_ROUTE,
+  BLOG_INDEX_ROUTE,
+  BLOG_POST_ROUTE,
+  NOT_FOUND_ROUTE
+} from './routes';
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Route exact path={INDEX_ROUTE} component={() => <div>Site Index</div>} />
-      <Route exact path={BLOG_INDEX_ROUTE} component={() => <div>Blog Index</div>} />
-      <Route exact path={BLOG_POST_ROUTE} component={() => <div>Blog Post</div>} />
+      <Switch>
+        <Route exact path={INDEX_ROUTE} component={() => <div>Site Index</div>} />
+        <Route exact path={BLOG_INDEX_ROUTE} component={() => <div>Blog Index</div>} />
+        <Route exact path={BLOG_POST_ROUTE} component={() => <div>Blog Post</div>} />
+        <Route exact path={CONTACT_ROUTE} component={() => <div>Contact</div>} />
+        <Route exact path={NOT_FOUND_ROUTE} component={() => <div>404 - Page Not Found</div>} />
+        <Route path={INDEX_ROUTE} render={() => <Redirect to={NOT_FOUND_ROUTE} />} />
+      </Switch>
     </BrowserRouter>
   );
 };
--- src/routing/routes.ts
+++ src/routing/routes.ts
@@ -1,3 +1,8 @@
 export const INDEX_ROUTE = '/';
+
 export const BLOG_INDEX_ROUTE = '/blog';
 export const BLOG_POST_ROUTE = '/blog/:id';
+
+export const CONTACT_ROUTE = '/contact';
+
+export const NOT_FOUND_ROUTE = '/404';
