d5571509008d2f30dd1f37893f7d147716df5bc2
Frederick Johnson <fritzj96@gmail.com>
2021-05-23 12:29:57 -0500

    more navigation progress

--- src/routing/Router.tsx
+++ src/routing/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { MainLayout } from '../shared';
+import { FourZeroFour } from '../pages';
 import {
   INDEX_ROUTE,
   CONTACT_ROUTE,
@@ -20,7 +20,7 @@ export const Router = () => {
         <Route exact path={BLOG_POST_ROUTE} component={() => <div>Blog Post</div>} />
         <Route exact path={CONTACT_ROUTE} component={() => <div>Contact</div>} />
         <Route exact path={HIRE_ME_ROUTE} component={() => <div>Contact</div>} />
-        <Route exact path={NOT_FOUND_ROUTE} component={() => <MainLayout />} />
+        <Route exact path={NOT_FOUND_ROUTE} component={() => <FourZeroFour />} />
         <Route path={INDEX_ROUTE} render={() => <Redirect to={NOT_FOUND_ROUTE} />} />
       </Switch>
     </BrowserRouter>
